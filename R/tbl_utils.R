bdbc4eeafd23a516a2da41150e3dc879ebc0227c
Jared Lander <info@jaredlander.com>
2016-07-31 17:51:13 -0400

    Hong's fix for the parallel problem.

--- R/tbl_utils.R
+++ R/tbl_utils.R
@@ -20,24 +20,15 @@ varTypes <- function(xdf, vars=NULL)
 
 
 # generate a new Xdf data source with file pointing to a random file, other parameters taken from input data source
-#' @title newTbl
-#' @description Makes a new tbl
-#' @details Wasn't exported and that was causing issues
-#' @author Jared P. Lander
-#' @export
-#' @param xdf Name of xdf
-#' @param fileSystem The file system we are on
-#' @return Not sure
-#' 
-newTbl <- function(xdf=NULL, fileSystem=rxGetFileSystem(xdf))
+newTbl <- function(xdf=NULL, fileSystem=rxGetFileSystem(xdf), tblDir=tempdir())
 {
     fname <- if(inherits(fileSystem, "RxNativeFileSystem"))
-        tempfile(fileext=".xdf")
+        tempfile(tmpdir=tblDir, fileext=".xdf")
     else if(inherits(fileSystem, "RxHdfsFileSystem"))
     {
         # ensure HDFS temporary directory exists
         makeHdfsTempDir()
-        file.path(hdfsTempDir, basename(tempfile(fileext=".xdf")), fsep="/")
+        file.path(.dxOptions$hdfsTempDir, basename(tempfile(pattern="xdfTbl")), fsep="/")
     }
     else stop("unknown file system")
 
@@ -52,17 +43,10 @@ newTbl <- function(xdf=NULL, fileSystem=rxGetFileSystem(xdf))
 
 
 # delete one or more xdf tbls (vectorised)
-#' @title deleteTbl
-#' @description Drops XDF tables
-#' @details This was nto exported so it could not be seen
-#' @author Jared P. Lander
-#' @export
-#' @param xdf A character string specifying the table to drop
-#' @return Not sure
 deleteTbl <- function(xdf)
 {
     if(is.character(xdf))
-        stop("must supply xdf file or list of xdf files")
+        stop("must supply xdf data source or list of data sources")
     if(!is.list(xdf))
         xdf <- list(xdf)
     lapply(xdf, function(xdf) {
@@ -71,78 +55,16 @@ deleteTbl <- function(xdf)
         if(inherits(filesystem, "RxNativeFileSystem"))
         {
             # use unlink because file.remove can't handle directories on Windows
+            # (composite xdfs are directories)
             if(file.exists(filename)) unlink(filename, recursive=TRUE)
         }
         else if(inherits(filesystem, "RxHdfsFileSystem"))
         {
-            # files in HDFS are always composite
-            rxHadoopRemoveDir(filename)
+            # xdf files in HDFS are always composite
+            rxHadoopRemoveDir(filename, skipTrash=TRUE, intern=TRUE)
         }
         else stop("unknown file system, cannot remove file")
     })
     invisible(NULL)
 }
 
-
-# create the temporary directory in HDFS
-# must run this every time we create a new tbl, because tempdir in HDFS is not guaranteed to exist
-makeHdfsTempDir <- function()
-{
-    hdfsTempDir <- .dxOptions$hdfsTempDir
-    # create temp directory in HDFS
-    # this behaviour is technically undocumented, but better than blindly trying to create dir
-    if(rxHadoopListFiles(hdfsTempDir) == 1)
-        rxHadoopMakeDir(hdfsTempDir)
-    .dxOptions$hdfsTempDirCreated <- TRUE
-    NULL
-}
-
-
-# environment for storing options
-.dxOptions <- new.env(parent=emptyenv())
-
-
-.dxInit <- function()
-{
-    # set the HDFS temporary directory
-    hdfsTempDir <- tempfile(pattern="dxTmp", tmpdir=RxHadoopMR()@hdfsShareDir)
-    .dxOptions$hdfsTempDir <- gsub("\\", "/", hdfsTempDir, fixed=TRUE)
-    .dxOptions$hdfsTempDirCreated <- FALSE
-    .dxOptions$rowsPerRead <- 500000
-
-    defaultFS <- rxGetFileSystem()
-    if(inherits(defaultFS, "RxHdfFileSystem"))
-        makeHdfsTempDir()
-
-    # store dplyr version
-    .dxOptions$dplyrVersion <- packageVersion("dplyr")
-
-    NULL
-}
-
-
-.dxFinal <- function(e)
-{
-    # remove the HDFS temporary directory
-    if(e$hdfsTempDirCreated)
-        rxHadoopRemoveDir(e$hdfsTempDir, skipTrash=TRUE)
-    NULL
-}
-
-
-dxOptions <- function(...)
-{
-    if(nargs() == 0)
-        return(as.list(.dxOptions))
-    dots <- list(...)
-    nams <- names(dots)
-    opts <- names(.dxOptions)
-    if(!all(nams %in% opts))
-    {
-        badOpts <- nams[!(nams %in% opts)]
-        stop("invalid dplyrXdf option(s): ", paste(badOpts, collapse=", "))
-    }
-    for(o in nams)
-        .dxOptions[[o]] <- dots[[o]]
-    as.list(.dxOptions)
-}
